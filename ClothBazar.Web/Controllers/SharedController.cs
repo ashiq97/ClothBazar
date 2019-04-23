013e902638203ffc3b91a1f0990c812a7c7a4d90
ashiq97 <ashiq070shishir@gmail.com>
2019-04-23 15:38:52 +0600

    Category Images upload

---
+++ ClothBazar.Web/Controllers/SharedController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class SharedController : Controller
+    {
+        // GET: Shared
+        public JsonResult UploadImage()
+        {
+            var result = new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+
+            try
+            {
+                var file = Request.Files[0];
+                //       var fileName = file.FileName;
+                var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+
+                var path = Path.Combine(Server.MapPath("~/content/images/"), fileName);
+
+                file.SaveAs(path);
+
+                //result.Data = new { Success = true, ImageUrl = path };
+                result.Data = new { Success = true, ImageUrl = string.Format("/content/images/{0}",fileName) };
+
+            }
+            catch (Exception ex)
+            {
+                result.Data = new { Success = false, Message = ex.Message };
+
+            }
+            return result;
+        }
+    }
+}
